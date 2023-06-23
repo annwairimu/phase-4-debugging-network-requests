3e30c5cc98e812f48e3ce6260121cd3c8230c547
Ann Wangui Wairimu <ann99wangui@gmail.com>
2023-06-23 14:12:32 +0300

    done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,28 @@
 Rails.application.routes.draw do
-  resources :movies, only: [:index]
+  resources :movies, only: [:index, :create]
+end
+```
+
+Then, add a controller method:
+
+```rb
+class MoviesController < ApplicationController
+
+  def create
+    movie = Movie.create(movie_params)
+    render json: movie, status: :created
+  end
+  
+  private
+  
+  def movie_params
+    params.permit(:title, :year, :length, :director, :description, :poster_url, :category, :discount, :female_director)
+  end
+  ```
+
+  def index
+    movies = Movie.all
+    render json: movies
+  end
+
 end
